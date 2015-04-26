e97126c15bb9022389367d7c4152a72c502e2473
DEADB17 <DEADB17@gmail.com>
2015-04-26 13:12:39 -0400

    converts tabs to spaces

--- create.js
+++ create.js
@@ -5,12 +5,12 @@ var trans = require('./transition');
 function errorFn(code) { throw new TypeError(code); }
 
 module.exports = function (table, currentState, error) {
-	error = error || errorFn;
-	return function transition(event) {
-		if (arguments.length > 0) {
-			currentState = trans(error, table, event, currentState);
-			return transition;
-		}
-		return currentState;
-	};
+    error = error || errorFn;
+    return function transition(event) {
+        if (arguments.length > 0) {
+            currentState = trans(error, table, event, currentState);
+            return transition;
+        }
+        return currentState;
+    };
 };
