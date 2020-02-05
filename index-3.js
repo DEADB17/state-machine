e8214d65accd66b85d9a651acec7794e725052bc
DEADB17 <DEADB17@gmail.com>
2020-02-05 12:57:43 -0500

    Remove store and chained go

--- index-3.js
+++ index-3.js
@@ -3,43 +3,36 @@
 
 /** @typedef {string} Vertex */
 
-/** @typedef {any} Payload */
-
 /** @typedef {{from: Vertex, to: Vertex}} Step */
 
 /**
- * @template Store
+ * @template P
  * @callback Edge
- * @arg {Store} store
- * @arg {Payload} [payload]
+ * @arg {P} payload
  * @arg {Step} [step]
- * @return {Store}
  */
 
 /**
- * @template Store
- * @typedef {{[from: string]: {[to: string]: Edge<Store>}}} Graph
+ * @template P
+ * @typedef {{[from: string]: {[to: string]: Edge<P>}}} Graph
  */
 
 /**
- * @template Store
- * @arg {Graph<Store>} graph
+ * @template P
+ * @arg {Graph<P>} graph
  * @arg {Vertex} from
- * @arg {Store} store
  */
-function create(graph, from, store) {
+function createMachine(graph, from) {
   /** @type {Step} */
   const step = {from: from, to: ''};
   /**
    * @arg {Vertex} to
-   * @arg {Payload} [payload]
+   * @arg {P} payload
    */
   function go(to, payload) {
     step.to = to;
-    // eslint-disable-next-line no-param-reassign
-    store = graph[step.from][to](store, payload, step);
+    graph[step.from][to](payload, step);
     step.from = to;
-    return go;
   }
 
   return go;
@@ -49,23 +42,34 @@ function create(graph, from, store) {
 // tests
 // /////////////////////////////////////////////////////////////////////////////
 
-/** @type {Edge<Array<[number, string, string]>>} */
-function trans(store, payload, {from, to}) {
-  store.push([payload, from, to]);
-  return store;
+/** @typedef {{counter: number, stack: [number, string, string][]}} Store */
+/** @typedef {[Store, string]} Payload */
+
+/**
+ * @arg {Payload} payload
+ * @arg {Step} step
+ */
+function trans(payload, {from, to}) {
+  payload[0].counter += 1;
+  payload[0].stack.push([payload[0].counter, from, to]);
 }
-/** @type {Edge<Array<[number, string, string]>>} */
-function done(store, payload, step) {
-  trans(store, payload, step);
-  console.log(store);
-  return [];
+/**
+ * @arg {Payload} payload
+ * @arg {Step} step
+ */
+function done(payload, step) {
+  trans(payload, step);
+  console.log(payload);
 }
 const graph = {
   a: { b: trans },
   b: { a: trans, c: done },
 };
 const from = 'a';
-/** @type {Array<any>} */
-const store = [];
-const go = create(graph, from, store);
-go('b', 1)('a', 2)('b', 3)('c', 4);
+/** @type {Store} */
+const store = {counter: 0, stack: []};
+const go = createMachine(graph, from);
+go('b', [store, 'click']);
+go('a', [store, 'click']);
+go('b', [store, 'click']);
+go('c', [store, 'click']);
